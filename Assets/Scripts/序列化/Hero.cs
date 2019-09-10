d73c71182c183043cccb28a977d61edb907b58d9
jiang jie <2822569160@qq.com>
2019-09-11 00:00:34 +0800

    序列化与反序列化
    把类里面的数据存在二进制文件中

---
+++ Assets/Scripts/序列化/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Hero
+{
+    public Hero()
+    {
+        
+    }
+
+    #region private
+    
+    private int id;
+    private int attack;
+    private int defence;
+    private string name;
+    
+    #endregion
+
+    #region public
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+        set { attack = value; }
+    }
+
+    public int Defence
+    {
+        get { return defence; }
+        set { defence = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/序列化/SerializationTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SerializationTest : MonoBehaviour
+{
+    private Hero heroInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroInstance = new Hero();
+
+        heroInstance.Id = 1;
+        heroInstance.Attack = 100;
+        heroInstance.Defence = 1000;
+        heroInstance.Name = "jj";
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Save(Serialize)", GUILayout.Width(200)))
+        {
+            //创建文件流
+            FileStream fileStream = new FileStream("HeroData", FileMode.Create);
+            
+            //创建格式化器
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            try
+            {
+                binaryFormatter.Serialize(fileStream, this.heroInstance);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                //关闭文件流
+                fileStream.Close();
+                
+                //为了演示反序列化的结果，存储完数据后 将实例清空
+                heroInstance = null;
+            }
+        }
+
+        if (GUILayout.Button("Load(Deserialize)", GUILayout.Width(200)))
+        {
+            //从硬盘中读取流的内容
+            FileStream fileStream = new FileStream("HeroData", FileMode.Open);
+
+            try
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                this.heroInstance = (Hero) binaryFormatter.Deserialize(fileStream);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to Deserialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fileStream.Close();
+                Debug.Log(this.heroInstance.Id.ToString());
+                Debug.Log(this.heroInstance.Attack.ToString());
+                Debug.Log(this.heroInstance.Defence.ToString());
+                Debug.Log(this.heroInstance.Name);
+            }
+        }
+    }
+}
+
+
